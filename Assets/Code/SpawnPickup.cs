99a4ad274e67737de37af9d523a5367b1e8b6f15
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2023-08-03 17:40:06 -0300

    spawnPickup objectPolling

--- Assets/Code/SpawnPickup.cs
+++ Assets/Code/SpawnPickup.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class SpawnPickup : MonoBehaviour{
-
+public class SpawnPickup : MonoBehaviour
+{
     [Header("Values")]
     [SerializeField] private Vector3[] _spawnPoints;
     [SerializeField] private float _delayBetweenSpawns;
@@ -22,18 +22,34 @@ public class SpawnPickup : MonoBehaviour{
     private byte _currentBoxAmount;
     private List<Vector3> _currentAvailable;
     private Coroutine _autoSpawnCoroutine;
+    private BulletPickup[] _bullets;
+    private BulletPickup _bulletAvailable;
 
     private void Awake()
     {
         _delay = new WaitForSeconds(_delayBetweenSpawns);
         _currentAvailable = new List<Vector3>(_spawnPoints);
         _spawnByDelay = _startWithAutomaticSpawns;
+        _bullets = new BulletPickup[_maxAmmoPickupsInScene];
+
+        for (int i = 0; i < _maxAmmoPickupsInScene; i++)
+        {
+            _bullets[i] = Instantiate(_bulletPickupPrefab, null).GetComponent<BulletPickup>();
+            _bullets[i].UpdateState(false);
+            _bullets[i].OnCollect += OnPickupCollect;
+            _bullets[i].name = $"BulletPickup {i}";
+        }
+
+        _bulletAvailable = _bullets[0];
+        for (int i = 0; i < _maxAmmoPickupsInScene - 1; i++) _bullets[i].SetNextInLine(_bullets[i + 1]);
+        _bullets[_maxAmmoPickupsInScene - 1].SetNextInLine(null);
+
         UpdateAutoSpawn();
     }
 
     private void UpdateAutoSpawn()
     {
-        if(_spawnByDelay && _autoSpawnCoroutine == null)
+        if (_spawnByDelay && _autoSpawnCoroutine == null)
         {
             _autoSpawnCoroutine = StartCoroutine(SpawnDelay());
         }
@@ -59,7 +75,9 @@ public class SpawnPickup : MonoBehaviour{
     {
         if (_currentBoxAmount < _maxAmmoPickupsInScene)
         {
-            Instantiate(_bulletPickupPrefab, GetValidSpawnPoint(), Quaternion.identity);
+            BulletPickup temp = GetBullet();
+            temp.SetSpawnPointUsed(GetValidSpawnPoint());
+            temp.gameObject.SetActive(true);
             _currentBoxAmount++;
         }
     }
@@ -71,11 +89,19 @@ public class SpawnPickup : MonoBehaviour{
         return temp;
     }
 
+    private void OnPickupCollect(Vector3 spawnPoint)
+    {
+        _currentAvailable.Add(spawnPoint);
+        _currentBoxAmount--;
+        UpdateAutoSpawn();
+        Debug.Log("collect");
+    }
+
     private void OnValidate()
     {
         Color[] temp = _colorPoints;
         _colorPoints = new Color[_spawnPoints.Length];
-        for (int i =  0; i < _colorPoints.Length; i++)
+        for (int i = 0; i < _colorPoints.Length; i++)
         {
             if (i >= temp.Length) break;
             _colorPoints[i] = temp[i];
@@ -92,5 +118,37 @@ public class SpawnPickup : MonoBehaviour{
                 Gizmos.DrawSphere(transform.position + _spawnPoints[i], _gizmoSize);
             }
         }
+    }
+
+    private BulletPickup GetBullet()
+    {
+        BulletPickup temp;
+        if (_bulletAvailable)
+        {
+            temp = _bulletAvailable;
+            _bulletAvailable = _bulletAvailable.NextInPoolList;
+        }
+        else
+        {
+            //dessa forma sepre tera uma sequencia de balas possiveis de serem usadas
+            for (int i = 0; i < _maxAmmoPickupsInScene; i++)
+            {
+                if (!_bullets[i].isActiveAndEnabled)
+                {
+                    _bullets[i].SetNextInLine(_bulletAvailable);
+                    _bulletAvailable = _bullets[i];
+                }
+            }
+            temp = _bulletAvailable;
+            if (_bulletAvailable) _bulletAvailable = _bulletAvailable.NextInPoolList;
+        }
+        return temp;
+        //modo padrão de busca por disponivel
+        //for(int i = 0; i < _bullets.Length; i++) {
+        //    if(!_bullets[i].isActiveAndEnabled) {
+        //        temp = _bullets[i];
+        //        break;
+        //    }
+        //}
     }
 }
