dd409f31db94d31ab71caf0e994984d895a5a319
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2023-08-03 18:09:08 -0300

    Bullet Implementation

--- Assets/Code/Bullet.cs
+++ Assets/Code/Bullet.cs
@@ -3,9 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour {
+    [SerializeField] private float _tickFrequency;
+    [SerializeField] private float _speed;
+
+    //[SerializeField] private Vector3 _dir;
+    //[SerializeField] private Transform _init;
+
+    //private WaitForSeconds _delay;
+    //private Coroutine _moveCoroutine;
+    //private Vector3 _currentDirection;
+    private Rigidbody _rb;
+
+    private void Awake()
+    {
+        //_delay = new WaitForSeconds(_tickFrequency);
+        _rb = GetComponent<Rigidbody>();
+    }
 
     public void Shoot(Vector3 initialPos, Vector3 direction) {
-
+        transform.position = initialPos;
+        //_currentDirection = direction;
+        if (this.isActiveAndEnabled) _rb.velocity = direction * _speed;
+        //if(_moveCoroutine == null) _moveCoroutine = StartCoroutine(Movment());
     }
+
+    //IEnumerator Movment()
+    //{
+    //    while (this.isActiveAndEnabled)
+    //    {
+    //        transform.position += _speed * _tickFrequency * _currentDirection;
+    //        yield return _delay;
+    //    }
+    //    _moveCoroutine = null;
+    //}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        PlayerHealth player = collision.collider.GetComponent<PlayerHealth>();
+        if (player) player.Die();
+        gameObject.SetActive(false);
+        _rb.velocity = Vector3.zero;
+    }
+
+    //[ContextMenu("Test")]
+    //private void DebugShoot()
+    //{
+    //    gameObject.SetActive(true);
+    //    Shoot(_init.position, _dir);
+    //}
 
 }
