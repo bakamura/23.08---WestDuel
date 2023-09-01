0bb80e2abd78024931c1d8083069fc50f9b7ca08
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2023-09-01 09:50:13 -0300

    Revert "PlayerShoot Implementation"
    This reverts commit c4f473d3e643c910fb466a01b69162df36d5a626.

--- Assets/Code/PlayerShoot.cs
+++ Assets/Code/PlayerShoot.cs
@@ -4,58 +4,12 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour {
 
-    [Header("Parameters")]
-
-    [SerializeField] private GameObject _bulletPrefab;
-    [SerializeField] private Transform _firePoint;
-    private bool _hasBullet = false;
-
-    [Header("Inputs")]
-
-    [SerializeField] private KeyCode _keyShoot;
-    [SerializeField] private LayerMask _mouseElevatedReceiver;
-
-    [Header("Cache")]
-
-    private bool _canInput = false;
-    private Bullet[] _bulletPool = new Bullet[2];
-    private Camera _cam;
-
-    private void Awake() {
-        for(int i = 0; i < _bulletPool.Length; i++) _bulletPool[i] = Instantiate(_bulletPrefab, Vector3.zero, Quaternion.identity).GetComponent<Bullet>();
-        _cam = Camera.main;
-
-        _canInput = true; // test
-        _hasBullet = true; // test
-    }
-
-    private void Update() {
-        if (_canInput && _hasBullet && Input.GetKeyDown(_keyShoot)) Shoot();
-    }
-
-    private void Shoot() {
-        Physics.Raycast(_cam.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, Mathf.Infinity, _mouseElevatedReceiver, QueryTriggerInteraction.Collide);
-
-        foreach (Bullet bullet in _bulletPool) {
-            if (!bullet.gameObject.activeSelf) {
-                bullet.gameObject.SetActive(true);
-                bullet.Shoot(_firePoint.position, hit.point);
-                //_hasBullet = false;
-                break;
-            }
-        }
-    }
-
     public void SetActive(bool isActive) {
-        _canInput = isActive;
-    }
 
-    public bool CheckBullet() {
-        return _hasBullet;
     }
 
     public void GetBullet() {
-        _hasBullet = true;
+
     }
 
 }
