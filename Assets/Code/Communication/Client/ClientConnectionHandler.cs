2194c25cbbe7470ca71b64a98a20c19f95e6091d
Vinicius Bologna Natal <viniciusbolognanatal@gmail.com>
2023-09-03 09:55:02 -0300

    ClientConnectionHandler

--- Assets/Code/Communication/Client/ClientConnectionHandler.cs
+++ Assets/Code/Communication/Client/ClientConnectionHandler.cs
@@ -1,9 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using UnityEngine;
 
-public static class ClientConnectionHandler {
-
+public static class ClientConnectionHandler
+{
     // Stores UDP Client + Server's IPEndPoint
+    public static List<MovableObjectData> PlayersList = new List<MovableObjectData>();
+    public static List<MovableObjectData> BulletsList = new List<MovableObjectData>();
+    public static List<BulletPickup> AmmoBoxList = new List<BulletPickup>();
 
+    [System.Serializable]
+    public struct MovableObjectData
+    {
+        public GameObject Object;
+        public Rigidbody Rigidbody;
+
+        public MovableObjectData(GameObject obj)
+        {
+            Object = obj;
+            Rigidbody = Object.GetComponent<Rigidbody>();
+        }
+    }
+
+    public const int Port = 11000;
+    public static IPEndPoint ServerEndPoint { get { return ServerEndPoint; } set { } }
+    public static UdpClient UdpClient = new UdpClient(Port);
+
+    public static void SetIpEndPoint(string IpAddress)
+    {
+        if (IPAddress.TryParse(IpAddress, out IPAddress address))
+        {
+            ServerEndPoint = new IPEndPoint(address, Port);
+        }
+        else
+        {
+            Debug.LogError($"the Address {IpAddress} is invalid");
+        }
+    }
 }
