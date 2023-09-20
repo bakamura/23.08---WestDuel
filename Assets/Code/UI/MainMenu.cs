e6b86780a64b071cbed4456dba102b70c52e5a8b
Pedro Kanzaki <ra00297288@pucsp.edu.br>
2023-09-20 09:48:43 -0300

    All Local Methods Fixed
    starting to test LAN dependant methods

--- Assets/Code/UI/MainMenu.cs
+++ Assets/Code/UI/MainMenu.cs
@@ -13,8 +13,9 @@ public class MainMenu : Menu {
     [Header("Menus")]
 
     [SerializeField] private CanvasGroup _mainMenu;
+    [SerializeField] private CanvasGroup _initialMenu;
     [SerializeField] private CanvasGroup _joinMenu;
-    [SerializeField] private CanvasGroup _hostMenu;
+    [SerializeField] private CanvasGroup _lobbyMenu;
     [SerializeField] private RectTransform _settingsMenu;
     [SerializeField] private Vector2 _settingsMenuActivePos;
     [SerializeField] private Vector2 _settingsMenuDeactivePos;
@@ -23,15 +24,16 @@ public class MainMenu : Menu {
 
     [SerializeField] private TextMeshProUGUI _ipInput;
 
-    [Header("Host Game")]
+    [Header("IP")]
 
-    private TextMeshProUGUI _ipSelf;
-    private IPEndPoint _hostIp;
-    private IPEndPoint _joinerIp;
+    [SerializeField] private TextMeshProUGUI _ipSelfText;
+    [SerializeField] private TextMeshProUGUI _ipOtherText;
+    private IPEndPoint _ipSelf;
+    private IPEndPoint _ipOther;
 
     [Header("Cache")]
 
-    private UdpClient _udpClient = new UdpClient(11000);
+    private UdpClient _udpClient = new UdpClient(10000);
     private IPEndPoint _ipEpCache;
     private MemoryStream _mStream;
     private BinaryFormatter _bFormatter = new BinaryFormatter();
@@ -44,27 +46,39 @@ public class MainMenu : Menu {
     private const string LEAVE_LOBBY = "LEAVE";
 
     private void Awake() {
-        _ipSelf.text = Dns.GetHostEntry(Dns.GetHostName()).AddressList.First(f => f.AddressFamily == AddressFamily.InterNetwork).ToString();
+        _currentUi = _initialMenu;
+        _ipSelf = new IPEndPoint(Dns.GetHostEntry(Dns.GetHostName()).AddressList.First(f => f.AddressFamily == AddressFamily.InterNetwork), 10000);
+
+        string[] ipText = _ipSelf.ToString().Split(':');
+        _ipSelfText.text = ipText[0];
     }
 
-    public void OpenMainMenu() {
-        OpenUIFade(_mainMenu);
+    public void OpenInitialMenu() {
+        OpenUIFade(_initialMenu);
     }
 
     public void OpenJoinMenu() {
-        if (_hostIp != null) {
+        _currentUi = _initialMenu.interactable ? _initialMenu : _mainMenu;
+        if (_ipOther != null) {
+            _mStream = new MemoryStream();
             _bFormatter.Serialize(_mStream, LEAVE_LOBBY);
-            _udpClient.Send(_mStream.ToArray(), _mStream.ToArray().Length, _hostIp);
+            _udpClient.Send(_mStream.ToArray(), _mStream.ToArray().Length, _ipOther);
         }
-        _threadC.Abort();
+        if (_threadC != null) _threadC.Abort();
         _threadC = new Thread(Joining);
         OpenUIFade(_joinMenu);
     }
 
+    public void OpenMainMenu() {
+        OpenUIFade(_mainMenu);
+        _currentUi = _joinMenu;
+    }
+
     public void OpenHostMenu() {
-        _threadC.Abort();
+        if (_threadC != null) _threadC.Abort();
         _threadC = new Thread(Hosting);
-        OpenUIFade(_joinMenu);
+        _currentUi = _mainMenu;
+        OpenUIFade(_lobbyMenu);
     }
 
     public void OpenSettingsMenu() {
@@ -72,55 +86,68 @@ public class MainMenu : Menu {
     }
 
     public void JoinGame() {
+        _mStream = new MemoryStream();
         _bFormatter.Serialize(_mStream, JOIN);
-        _udpClient.Send(_mStream.ToArray(), _mStream.ToArray().Length, new IPEndPoint(IPAddress.Parse(_ipInput.text), 11000));
-    }
-
-    public void HostGame() {
-        if (_joinerIp != null) {
-            _bFormatter.Serialize(_mStream, JOIN);
-            _udpClient.Send(_mStream.ToArray(), _mStream.ToArray().Length, new IPEndPoint(IPAddress.Parse(_ipInput.text), 11000));
-        }
+        _udpClient.Send(_mStream.ToArray(), _mStream.ToArray().Length, new IPEndPoint(IPAddress.Parse(_ipInput.text), 10000));
     }
 
     public void StartGame() {
+        _mStream = new MemoryStream();
         _bFormatter.Serialize(_mStream, START);
-        _udpClient.Send(_mStream.ToArray(), _mStream.ToArray().Length, _joinerIp);
+        _udpClient.Send(_mStream.ToArray(), _mStream.ToArray().Length, _ipOther);
     }
 
     public void CopySelfIp() {
-        GUIUtility.systemCopyBuffer = _ipSelf.text;
+        _ipOther = _ipEpCache;
+        string[] ipText = _ipSelf.ToString().Split(':');
+        GUIUtility.systemCopyBuffer = ipText[0];
     }
 
     private void Hosting() {
-        if (_joinerIp == null) {
+        if (_ipOther == null) {
             _mStream = new MemoryStream(_udpClient.Receive(ref _ipEpCache));
             string str = (string)_bFormatter.Deserialize(_mStream);
             if (str == JOIN) {
-                _joinerIp = _ipEpCache;
+                _ipOther = _ipEpCache;
+                _mStream = new MemoryStream();
                 _bFormatter.Serialize(_mStream, JOIN_SUCCESS);
-                _udpClient.Send(_mStream.ToArray(), _mStream.ToArray().Length, _joinerIp);
-            } else if (str == START_SUCCESS) {
-                ServerConnectionHandler.players[0].ip = _joinerIp; // Instantiate before
+                _udpClient.Send(_mStream.ToArray(), _mStream.ToArray().Length, _ipOther);
+            }
+            else if (str == START_SUCCESS) {
+                ServerConnectionHandler.players[0].ip = _ipOther; // Instantiate before
                 SceneManager.LoadScene(1);
-            } else if (str == LEAVE_LOBBY) {
-                _joinerIp = null;
+            }
+            else if (str == LEAVE_LOBBY) {
+                _ipOther = null;
             }
         }
     }
 
     private void Joining() {
-        if (_hostIp == null) {
+        if (_ipOther == null) {
             _mStream = new MemoryStream(_udpClient.Receive(ref _ipEpCache));
             string str = (string)_bFormatter.Deserialize(_mStream);
-            if (str == JOIN_SUCCESS) _hostIp = _ipEpCache;
+            if (str == JOIN_SUCCESS) {
+                _currentUi = _mainMenu;
+                OpenUIFade(_lobbyMenu);
+
+                _ipOther = _ipEpCache;
+                string[] ipText = _ipOther.ToString().Split(':');
+                _ipOtherText.text = ipText[0];
+            }
             else if (str == START) {
+                _mStream = new MemoryStream();
                 _bFormatter.Serialize(_mStream, START_SUCCESS);
-                _udpClient.Send(_mStream.ToArray(), _mStream.ToArray().Length, new IPEndPoint(IPAddress.Parse(_ipInput.text), 11000));
-                ClientConnectionHandler.ServerEndPoint = _hostIp;
+                _udpClient.Send(_mStream.ToArray(), _mStream.ToArray().Length, new IPEndPoint(IPAddress.Parse(_ipInput.text), 10000));
+                ClientConnectionHandler.ServerEndPoint = _ipOther;
                 SceneManager.LoadScene(1);
             }
         }
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+        print("left");
     }
 
     ~MainMenu() {
