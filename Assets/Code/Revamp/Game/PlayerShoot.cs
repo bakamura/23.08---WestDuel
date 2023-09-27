0ec3a50702fc80bc476e5707c814bc987882bf38
NakaVostro <ra00297288@pucsp.edu.br>
2023-09-26 23:31:02 -0300

    Player Scripts (Server) Finish (Probably)

--- Assets/Code/Revamp/Game/PlayerShoot.cs
+++ Assets/Code/Revamp/Game/PlayerShoot.cs
@@ -1,18 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.Events;
 
-public class PlayerShoot : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PlayerShoot : MonoBehaviour {
+
+    public UnityEvent<PlayerShoot> onShoot = new UnityEvent<PlayerShoot>();
+
+    [Header("Parameters")]
+
+    [SerializeField] private GameObject _bulletPrefab;
+    [SerializeField] private Transform _firePoint;
+    private bool _hasBullet = false;
+    private bool _canInput = false;
+
+    [Header("Cache")]
+
+    public const int MaxBulletAmount = 2;
+    private Bullet[] _bulletPool = new Bullet[MaxBulletAmount];
+    private Camera _cam;
+
+    private void Awake() {
+        for (int i = 0; i < _bulletPool.Length; i++) {
+            _bulletPool[i] = Instantiate(_bulletPrefab, Vector3.up * 256f, Quaternion.identity).GetComponent<Bullet>();
+            _bulletPool[i].owner = transform;
+        }
+        _cam = Camera.main;
+
+        _canInput = true; // Should be called by game initiator
+        _hasBullet = true; // Should be called by game initiator
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Shoot(Vector3 mouseWorldPos) {
+        if (_canInput && _hasBullet) {
+            foreach (Bullet bullet in _bulletPool) {
+                if (!bullet.gameObject.activeSelf) {
+                    bullet.gameObject.SetActive(true); // Not needed?
+                    bullet.Shoot(_firePoint.position, mouseWorldPos);
+                    _hasBullet = false;
+                    break;
+                }
+            }
+            onShoot?.Invoke(this);
+        }
     }
+
+    public void SetActive(bool isActive) {
+        _canInput = isActive;
+    }
+
+    public bool CheckBullet() {
+        return _hasBullet;
+    }
+
+    public void GetBullet() {
+        _hasBullet = true;
+    }
+
 }
